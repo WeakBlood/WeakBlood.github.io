f2f22ee3a80ec89a905b26737ca21821d0013f48
WeakBlood <123515315+WeakBlood@users.noreply.github.com>
2023-04-23 17:33:18 +0200

    Update Java.js

--- assets/js/Java.js
+++ assets/js/Java.js
@@ -23,7 +23,58 @@ function loadDarkMode() {
     }
   }
 
-  function copiaTesto() {
+function copiaTesto() {
     var testo = document.getElementById("testoDaCopiare").innerText;
     navigator.clipboard.writeText(testo);
   }
+
+  function submitNumber() {
+    let inputString = document.getElementById("inputNumber").value;
+    if (isBinaryString(inputString)){
+        let result = twoComplement(inputString);
+        document.getElementById("result").innerHTML = result;
+    }
+    else{
+        let decimalNumber = parseInt(inputString);
+        if (decimalNumber >= 0) {
+            let realBinary = decimalNumber.toString(2);
+            document.getElementById("result").innerHTML = realBinary;
+        } else {
+            let realBinary = Math.abs(decimalNumber).toString(2);
+            let result = twoComplement(realBinary);
+            document.getElementById("result").innerHTML = result;
+        }
+    }
+}
+
+
+function twoComplement(binaryString) {
+  let invertedString = "";
+  for (let i = 0; i < binaryString.length; i++) {
+      invertedString += binaryString[i] === "0" ? "1" : "0";
+  }
+  
+  let carry = 1;
+  let result = "";
+  for (let i = binaryString.length - 1; i >= 0; i--) {
+      let sum = carry + parseInt(invertedString[i]);
+      result = (sum % 2) + result;
+      carry = Math.floor(sum / 2);
+  }
+
+  return result;
+}
+
+function isBinaryString(string) {
+  for (let i = 0; i < string.length; i++) {
+      if (string[i] !== "0" && string[i] !== "1") {
+          return false;
+      }
+  }
+  return true;
+}
+
+
+
+
+
